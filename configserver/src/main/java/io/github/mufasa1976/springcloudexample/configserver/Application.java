2c4dcffdfb995657c0376860ff846e5528b04a82
mufasa1976 <mufasa1976@coolstuff.software>
2017-03-22 20:14:36 +0100

    try to register the configserver on eureka

--- configserver/src/main/java/io/github/mufasa1976/springcloudexample/configserver/Application.java
+++ configserver/src/main/java/io/github/mufasa1976/springcloudexample/configserver/Application.java
@@ -19,10 +19,12 @@ package io.github.mufasa1976.springcloudexample.configserver;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class Application {
 
   public static void main(String... args) {
