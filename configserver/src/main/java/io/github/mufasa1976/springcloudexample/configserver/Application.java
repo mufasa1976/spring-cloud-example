bc103da079a9d3f12b00650b9fc66e2e5987529e
mufasa1976 <mufasa1976@coolstuff.software>
2017-03-23 00:44:43 +0100

    just played with hystrix-dashboard

--- configserver/src/main/java/io/github/mufasa1976/springcloudexample/configserver/Application.java
+++ configserver/src/main/java/io/github/mufasa1976/springcloudexample/configserver/Application.java
@@ -21,10 +21,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
+@EnableHystrix
 public class Application {
 
   public static void main(String... args) {
